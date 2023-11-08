3c38094c07fb4520a84d7638c5de34bdb46200e3
Siva Chandan C <sivachandan1996@gmail.com>
2023-11-08 02:21:22 -0500

    Almost done with home page

--- static/script.js
+++ static/script.js
@@ -1,52 +1,75 @@
 // Throttle function to limit the function calls
 function throttle(fn, wait) {
-    var time = Date.now();
-    return function() {
+  var time = Date.now();
+  return function() {
       if ((time + wait - Date.now()) < 0) {
-        fn();
-        time = Date.now();
+          fn();
+          time = Date.now();
       }
-    }
   }
-  
-  // The actual scroll handling function
-  // The actual scroll handling function
+}
+
+// The actual scroll handling function
 function handleScroll() {
   let scrollPosition = window.pageYOffset;
   let sections = document.querySelectorAll("section");
 
   for (let i = 0; i < sections.length; i++) {
-    let section = sections[i];
-    let sectionTop = section.offsetTop;
+      let section = sections[i];
+      let sectionTop = section.offsetTop;
 
-    // Check if the section top is just at the viewport and if we are scrolling down
-    if (scrollPosition >= sectionTop) {
-      // Check if there is a next section to scroll into view
-      if (i < sections.length - 1) {
-        // Smoothly scroll to the next section without any offset
-        sections[i + 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
-        break; // Exit the loop
+      // Check if the section top is just at the viewport and if we are scrolling down
+      if (scrollPosition >= sectionTop) {
+          // Check if there is a next section to scroll into view
+          if (i < sections.length - 1) {
+              // Smoothly scroll to the next section without any offset
+              sections[i + 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
+              break; // Exit the loop
+          }
       }
-    }
   }
 }
 
-  
-  // Event listener for the scroll event with throttle
-  window.addEventListener('scroll', throttle(handleScroll, 1000));
+// Function to cycle through job titles
+const jobTitles = ["Engineer", "Analyst", "Consultant", "Scientist"];
+const jobTitleElement = document.getElementById("jobTitle");
+const staticTextElement = document.querySelector(".static-text");
+let currentIndex = 0;
 
-  // Function to open the About Me modal
-  function openAboutMeModal() {
+function cycleJobTitles() {
+    const currentTitle = jobTitles[currentIndex];
+    jobTitleElement.textContent = currentTitle;
+
+    // Calculate the width of the current title and set it as a fixed width for the container
+    const titleWidth = jobTitleElement.offsetWidth;
+    staticTextElement.style.width = titleWidth + "px";
+
+    currentIndex = (currentIndex + 1) % jobTitles.length;
+}
+
+// Initialize the text with the first job title
+cycleJobTitles();
+
+// Start the job title cycling
+setInterval(cycleJobTitles, 2000); // Adjust the interval (in milliseconds) as needed
+
+
+
+// Event listener for the scroll event with throttle
+window.addEventListener('scroll', throttle(handleScroll, 1000));
+
+// Function to open the About Me modal
+function openAboutMeModal() {
   document.getElementById('aboutMeModal').style.display = 'block';
-  }
+}
 
-  // Function to close the About Me modal
-  function closeAboutMeModal() {
+// Function to close the About Me modal
+function closeAboutMeModal() {
   document.getElementById('aboutMeModal').style.display = 'none';
-  }
+}
 
-  // Event listener for the "About Me" button click
-  document.getElementById('aboutMeButton').addEventListener('click', openAboutMeModal);
+// Event listener for the "About Me" button click
+document.getElementById('aboutMeButton').addEventListener('click', openAboutMeModal);
 
-  // Event listener for the close button in the modal
-  document.getElementById('closeAboutMe').addEventListener('click', closeAboutMeModal);
+// Event listener for the close button in the modal
+document.getElementById('closeAboutMe').addEventListener('click', closeAboutMeModal);
