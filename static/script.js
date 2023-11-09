222034a4fd06ce5db54f98e0ce0f416c2688f698
Siva Chandan C <sivachandan1996@gmail.com>
2023-11-08 22:19:50 -0500

    added a diaulouge box instead of modal

--- static/script.js
+++ static/script.js
@@ -2,11 +2,11 @@
 function throttle(fn, wait) {
   var time = Date.now();
   return function() {
-      if ((time + wait - Date.now()) < 0) {
-          fn();
-          time = Date.now();
-      }
-  }
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  };
 }
 
 // The actual scroll handling function
@@ -15,18 +15,18 @@ function handleScroll() {
   let sections = document.querySelectorAll("section");
 
   for (let i = 0; i < sections.length; i++) {
-      let section = sections[i];
-      let sectionTop = section.offsetTop;
+    let section = sections[i];
+    let sectionTop = section.offsetTop;
 
-      // Check if the section top is just at the viewport and if we are scrolling down
-      if (scrollPosition >= sectionTop) {
-          // Check if there is a next section to scroll into view
-          if (i < sections.length - 1) {
-              // Smoothly scroll to the next section without any offset
-              sections[i + 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
-              break; // Exit the loop
-          }
+    // Check if the section top is just at the viewport and if we are scrolling down
+    if (scrollPosition >= sectionTop) {
+      // Check if there is a next section to scroll into view
+      if (i < sections.length - 1) {
+        // Smoothly scroll to the next section without any offset
+        sections[i + 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
+        break; // Exit the loop
       }
+    }
   }
 }
 
@@ -37,14 +37,14 @@ const staticTextElement = document.querySelector(".static-text");
 let currentIndex = 0;
 
 function cycleJobTitles() {
-    const currentTitle = jobTitles[currentIndex];
-    jobTitleElement.textContent = currentTitle;
+  const currentTitle = jobTitles[currentIndex];
+  jobTitleElement.textContent = currentTitle;
 
-    // Calculate the width of the current title and set it as a fixed width for the container
-    const titleWidth = jobTitleElement.offsetWidth;
-    staticTextElement.style.width = titleWidth + "px";
+  // Calculate the width of the current title and set it as a fixed width for the container
+  const titleWidth = jobTitleElement.offsetWidth;
+  staticTextElement.style.width = `${titleWidth}px`;
 
-    currentIndex = (currentIndex + 1) % jobTitles.length;
+  currentIndex = (currentIndex + 1) % jobTitles.length;
 }
 
 // Initialize the text with the first job title
@@ -53,23 +53,21 @@ cycleJobTitles();
 // Start the job title cycling
 setInterval(cycleJobTitles, 2000); // Adjust the interval (in milliseconds) as needed
 
-
-
 // Event listener for the scroll event with throttle
 window.addEventListener('scroll', throttle(handleScroll, 1000));
 
-// Function to open the About Me modal
-function openAboutMeModal() {
-  document.getElementById('aboutMeModal').style.display = 'block';
+// Function to open the About Me dialog
+function openAboutMeDialog() {
+  document.getElementById('aboutMeDialog').showModal();
 }
 
-// Function to close the About Me modal
-function closeAboutMeModal() {
-  document.getElementById('aboutMeModal').style.display = 'none';
+// Function to close the About Me dialog
+function closeAboutMeDialog() {
+  document.getElementById('aboutMeDialog').close();
 }
 
 // Event listener for the "About Me" button click
-document.getElementById('aboutMeButton').addEventListener('click', openAboutMeModal);
+document.getElementById('aboutMeButton').addEventListener('click', openAboutMeDialog);
 
-// Event listener for the close button in the modal
-document.getElementById('closeAboutMe').addEventListener('click', closeAboutMeModal);
+// Event listener for the close button in the dialog
+document.getElementById('closeAboutMeButton').addEventListener('click', closeAboutMeDialog);
